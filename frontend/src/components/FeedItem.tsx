59ea1c3485e1b4becdf12d4c85f809daa82e9c93
Bohdan Dhzus <b.dhzus@yahoo.com>
2023-11-11 16:00:56 +0200

    Fix error with undefined in FeedItem

--- frontend/src/components/FeedItem.tsx
+++ frontend/src/components/FeedItem.tsx
@@ -1,8 +1,20 @@
 const FeedItem = ({ item }: { item: any }) => {
   return (
-    <div className="max-w-2xl mx-auto my-4 p-6 bg-white shadow overflow-hidden sm:rounded-lg">
-      <h3 className="text-2xl font-bold text-indigo-600 mb-2">{item.title}</h3>
-      <p className="text-gray-700 text-base">{item.description}</p>
+    <div>
+      {item ? (
+        <div className="max-w-2xl mx-auto my-4 p-6 bg-white shadow overflow-hidden sm:rounded-lg">
+          <h3 className="text-2xl font-bold text-indigo-600 mb-2">
+            {item.title}
+          </h3>
+          <p className="text-gray-700 text-base">{item.description}</p>
+        </div>
+      ) : (
+        <div className="max-w-2xl mx-auto my-4 p-6 bg-white shadow overflow-hidden sm:rounded-lg">
+          <h3 className="text-2xl font-bold text-indigo-600 mb-2">
+            Loading...
+          </h3>
+        </div>
+      )}
     </div>
   );
 };
